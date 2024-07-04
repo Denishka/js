3d68b81f391970fbbadbe7bc98a7379577ca327f
Denishka <denis.sabirzianow@yandex.ru>
2024-07-04 23:02:45 +0400

    ss

--- 02-array/app.js
+++ 02-array/app.js
@@ -4,14 +4,90 @@ const listElement = document.getElementById('list')
 
 console.log(inputElement.value)
 
+//const notes = ['1', '2']
+const notes = [
+  {
+    title: "Пу пу пу",
+    completed: false,
+  },
+  {
+    title: "two",
+    completed: true,
+  },
+]
+
+
+/*function render() {
+  for(let note of notes) {
+    listElement.insertAdjacentHTML('beforeend', 
+      getNoteTemplate(note)
+    )
+  }
+
+
+}
+render()*/
+
 createBtn.onclick = function () {
-    listElement.innerHTML =  `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${inputElement.value}</span>
-    <span>
-      <span class="btn btn-small btn-success">&check;</span>
-      <span class="btn btn-small btn-danger">&times;</span>
-    </span>
-  </li>
-  `
+  if(inputElement.value.length === 0){
+    return
+  }
+  const newNote ={
+    title: inputElement.value,
+    completed: false,
+  }
+  listElement.insertAdjacentHTML('beforeend', 
+    getNoteTemplate(newNote)
+
+)
+notes.push(newNote)
+render()
+inputElement.value = ""
+}
+
+function getNoteTemplate(note, index) {
+  return     `<li class="list-group-item d-flex justify-content-between align-items-center">
+  <span class="${note.completed ? "text-decoration-line-through" : ''}">${note.title}</span>
+  <span>
+    <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}" data-index="${index}" data-type="toggle">&check;</span>
+    <span class="btn btn-small btn-danger" data-type="remove" data-index="${index}">&times;</span>
+  </span>
+</li>
+`
+}
+
+/*const person = {
+  firstName: 'Vlad',
+  lastName: 'Min',
+  getFullName: function () {
+    console.log(person.firstName + ' ' + person.lastName)
+  }
+}
+
+console.log(person.year)*/
+
+function render() {
+  listElement.innerHTML = ""
+  for(let i = 0; i <notes.length; ++i) {
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
+  }
+
+
+}
+render()
+
+listElement.onclick = function (event) {
+  if (event.target.dataset.index) {
+    const index = parseInt(event.target.dataset.index)
+    const type = event.target.dataset.type
+
+    if (type === 'toggle') {
+      //console.log('toggle', index)
+      notes[index].completed = !notes[index].completed
+    } else if (type === 'remove') {
+      //console.log('remove', index)
+      notes.splice(index, 1)
+    }
+  }
+  render()
 }
